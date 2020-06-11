5d43dd9340b1ae97c1427bb90587edc4e47b23e7
Anguse <liljaharald@gmail.com>
2020-06-11 16:11:45 +0200

    real odometry ok

--- rob_ctrl/scripts/motor_controller.py
+++ rob_ctrl/scripts/motor_controller.py
@@ -3,11 +3,24 @@ import rospy
 from ctypes import *
 import sys
 import wiringpi
-from math import sin, cos, pi
+from math import sin, cos, pi, degrees 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 import tf
 
+# dc motor with 19:1 gear
+
+# CONSTANTS
+ENCODER = 128
+ENCODER_COUNTER = 4
+SAMPLE_TIME = 0.01
+GEAR_REDUCTION = 15.5/24.0 # adjust
+GEARBOX_RATIO = (24.0/1.0)*(1.0/2.5) # verify
+WHEEL_CIRC = 42.0*pi # adjust
+WHEELBASE_MM = 225.0
+PULSES_PER_REVOLUTION = ENCODER*ENCODER_COUNTER*GEAR_REDUCTION
+MM_PER_PULSE = WHEEL_CIRC/(PULSES_PER_REVOLUTION*GEARBOX_RATIO)
+PULSES_PER_MM = PULSES_PER_REVOLUTION*GEARBOX_RATIO/WHEEL_CIRC
 
 class TYPE_White_Board_TX(Structure):
     _fields_ = [('Status', c_ubyte),
@@ -40,8 +53,6 @@ class TYPE_White_Board_RX(Structure):
                 ('Position_M1', c_int)]
 
 # class for steering motors
-
-
 class Motorcontroller():
     def __init__(self):
         self.SPI_CHANNEL = 1
@@ -51,6 +62,9 @@ class Motorcontroller():
         self.pWhite_Board_TX = TYPE_White_Board_TX()
 
         # odom
+	self.deltaw = 0 # change since last odom update
+	self.delta0 = 0
+	self.delta1 = 0
         self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
         self.odom_broadcaster = tf.TransformBroadcaster()
         self.x = 0.0
@@ -63,9 +77,13 @@ class Motorcontroller():
 
     def update_odom(self):
         current_time = rospy.Time.now()
-        self.vx =
         # compute odometry in a typical way given the velocities of the robot
         dt = (current_time - self.last_time).to_sec()
+	self.vx = ((self.delta0+self.delta1)/2)/dt
+	self.vy = 0.0
+	self.vth = ((self.delta0-self.delta1)/(WHEELBASE_MM/1000))/dt # estimation a bit slow
+	rospy.loginfo(self.vth)
+
         delta_x = (self.vx * cos(self.th) - self.vy * sin(self.th)) * dt
         delta_y = (self.vx * sin(self.th) + self.vy * cos(self.th)) * dt
         delta_th = self.vth * dt
@@ -102,32 +120,21 @@ class Motorcontroller():
 
         # publish the message
         self.odom_pub.publish(odom)
-        rospy.loginfo(self.x)
-        rospy.loginfo(self.y)
-        rospy.loginfo(self.th)
         self.last_time = current_time
+	self.deltaw = 0
+	self.delta0 = 0
+	self.delta1 = 0
 
     def get_encoder_values(self):
         pass
 
-    def set_speed(self, speed_M0, speed_M1):
-        pWhite_Board_RX_update = TYPE_White_Board_RX()
-
+    def set_speed(self, speed_M1, speed_M0):
         self.pWhite_Board_TX.Status = 1
         self.pWhite_Board_TX.Set_Speed_M0 = speed_M0
         self.pWhite_Board_TX.Set_Speed_M1 = speed_M1
         self.pWhite_Board_TX.Heart_Beat = 1
         self.pWhite_Board_TX.Digital_Out = 0x00
 
-        # serialize data and send over SPI
-        buf = cast(byref(self.pWhite_Board_TX), POINTER(
-            c_char * sizeof(self.pWhite_Board_TX)))
-        retlen, retdata = wiringpi.wiringPiSPIDataRW(
-            self.SPI_CHANNEL, buf.contents.raw)
-        memmove(pointer(pWhite_Board_RX_update),
-                retdata, sizeof(pWhite_Board_RX_update))
-        print(pWhite_Board_RX_update.Position_M0)
-        self.pWhite_Board_RX = pWhite_Board_RX_update
 
     def reset_encoders(self):
         self.pWhite_Board_TX.Status = 0
@@ -163,45 +170,38 @@ class Motorcontroller():
                 retdata, sizeof(self.pWhite_Board_RX))
         print(self.pWhite_Board_RX.Position_M0)
 
+    def send_encoder_values(self):
+	pWhite_Board_RX_update = TYPE_White_Board_RX()
+	buf=cast(byref(self.pWhite_Board_TX), POINTER(c_char * sizeof(self.pWhite_Board_TX)))
+	retlen, retdata=wiringpi.wiringPiSPIDataRW(self.SPI_CHANNEL, buf.contents.raw)
+	memmove(pointer(pWhite_Board_RX_update), retdata, sizeof(pWhite_Board_RX_update))
+	diff0_mm = (pWhite_Board_RX_update.Position_M0 - self.pWhite_Board_RX.Position_M0)/PULSES_PER_MM
+	diff1_mm = (pWhite_Board_RX_update.Position_M1 - self.pWhite_Board_RX.Position_M1)/PULSES_PER_MM
+	self.deltaw += (diff0_mm-diff1_mm)/WHEELBASE_MM
+	self.delta0 += diff0_mm/1000 #meters
+	self.delta1 += diff1_mm/1000
+	self.pWhite_Board_RX = pWhite_Board_RX_update
+
 
 def main():
     rospy.init_node('rob_ctrl')
     controller = Motorcontroller()
     controller.reset_encoders()
-    r = rospy.Rate(1.0)
-    controller.set_speed(-30, -30)
+    controller.set_speed(30, 30)
+    r = rospy.Rate(20.0)
+    dist = 0
+    start_time = rospy.Time.now()
     while not rospy.is_shutdown():
-        controller.update_odom()
-        r.sleep()
-    '''
-	controller.set_speed(-30, -30)
-	while(controller.pWhite_Board_RX.Position_M0 > -10000):
-		buf=cast(byref(controller.pWhite_Board_TX), POINTER(
-		    c_char * sizeof(controller.pWhite_Board_TX)))
-		retlen, retdata=wiringpi.wiringPiSPIDataRW(
-		    controller.SPI_CHANNEL, buf.contents.raw)
-		memmove(pointer(controller.pWhite_Board_RX),
-		        retdata, sizeof(controller.pWhite_Board_RX))
-		print(controller.pWhite_Board_RX.Position_M0)
-	controller.set_speed(0, -30)
-	while(controller.pWhite_Board_RX.Position_M1 > -20000):
-		buf=cast(byref(controller.pWhite_Board_TX), POINTER(
-		    c_char * sizeof(controller.pWhite_Board_TX)))
-		retlen, retdata=wiringpi.wiringPiSPIDataRW(
-		    controller.SPI_CHANNEL, buf.contents.raw)
-		memmove(pointer(controller.pWhite_Board_RX),
-		        retdata, sizeof(controller.pWhite_Board_RX))
-		print(controller.pWhite_Board_RX.Position_M1)
-	controller.set_speed(-30, -30)
-	while(controller.pWhite_Board_RX.Position_M1 > -30000):
-		buf=cast(byref(controller.pWhite_Board_TX), POINTER(
-		    c_char * sizeof(controller.pWhite_Board_TX)))
-		retlen, retdata=wiringpi.wiringPiSPIDataRW(
-		    controller.SPI_CHANNEL, buf.contents.raw)
-		memmove(pointer(controller.pWhite_Board_RX),
-		        retdata, sizeof(controller.pWhite_Board_RX))
-		print(controller.pWhite_Board_RX.Position_M0)
-	'''
+	controller.send_encoder_values()
+	controller.update_odom()
+	t_pass = (rospy.Time.now() - start_time).to_sec()
+	if t_pass > 5 and t_pass < 8:
+		controller.set_speed(30, -30)
+	elif t_pass > 8 and t_pass < 13:
+		controller.set_speed(30, 30)
+	elif t_pass > 13:
+		break
+	r.sleep()
     return
 
 
